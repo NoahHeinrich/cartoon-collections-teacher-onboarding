53b18055321892c5b304069e4d97b6455b7d980d
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 15:27:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  # Your code here
+  []
 end
 
 def long_planeteer_calls(array)# code an argument here
