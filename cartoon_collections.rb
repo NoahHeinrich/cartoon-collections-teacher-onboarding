07c07fa18c4f499ac5a2411d1f336bc4344e3f4b
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 15:32:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any?
+  array.any?{ |word| word.length > 4 }
 end
 
 def find_the_cheese(array)# code an argument here
